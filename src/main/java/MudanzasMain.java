2ab5af58266dd5b65eac1af4e7b7248433553132
alberto <>
2024-03-25 20:26:06 +0100

    Clase voraz para hacer el algoritmo

--- src/main/java/MudanzasMain.java
+++ src/main/java/MudanzasMain.java
@@ -1,5 +1,11 @@
+import java.util.ArrayList;
+
 public class MudanzasMain {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+    }
+    ArrayList<float> voraz(ArrayList<float> pesos, float capacidad, float precio){
+        ArrayList sol=null;
+        return sol;
     }
 }
