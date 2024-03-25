f4f6572b011d8945420cdc8a118d2276b77bcf4e
alberto <>
2024-03-25 20:55:58 +0100

    Uso de Iterador y ArrayList. Falta el precio

--- src/main/java/MudanzasMain.java
+++ src/main/java/MudanzasMain.java
@@ -1,11 +1,24 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 
 public class MudanzasMain {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
-    ArrayList<float> voraz(ArrayList<float> pesos, float capacidad, float precio){
-        ArrayList sol=null;
+    ArrayList<float> voraz(ArrayList<Float> pesos, float capacidad, float precio){
+        float pactual=0;
+        Collections.sort(pesos);
+        Iterator<Float> it = pesos.iterator();
+        ArrayList<float> sol=null;
+        float aux;
+        //Bucle voraz:
+        while(pactual<capacidad && it.hasNext()){
+            aux=it.next();
+            if(aux <= capacidad - pactual)
+                sol.add(aux);
+            pesos.remove(aux);
+        }
         return sol;
     }
 }
