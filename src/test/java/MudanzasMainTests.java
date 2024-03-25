5f7f38b3f9d6ae7a460fdec90f30a4db65452209
vavaji <vavaji@alumni.uv.es>
2024-03-25 20:09:19 +0100

    He creado la clase test y hecho uno de prueba

---
+++ src/test/java/MudanzasMainTests.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+public class MudanzasMainTests {
+    @Test
+    public void testMain() {
+        MudanzasMain.main(null);
+        Assertions.assertTrue(true);
+    }
+}
