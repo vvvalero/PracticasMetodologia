594fbb116fe502b6aacf52e9cd72089cb57d0412
alberto <>
2024-03-26 17:47:34 +0100

    Cambios en voraz() y esSolucion()

--- src/main/java/MudanzasMain.java
+++ src/main/java/MudanzasMain.java
@@ -6,10 +6,10 @@ import java.util.Scanner;
 public class MudanzasMain {
     private static Scanner scanner = new Scanner(System.in);
 
-    public static int dameEspacioCamion(){
+    public static float dameEspacioCamion(){
         System.out.println("Introduzca peso máximo que se podrá cargar en el camión: ");
-        int i = scanner.nextInt();
-        return i;
+        float espacio = scanner.nextFloat();
+        return espacio;
     }
     public static float [] dameObjetos(){
         float [] objetos = new float[100];
@@ -26,31 +26,38 @@ public class MudanzasMain {
         return objetos;
     }
     public static void main(String[] args) {
-        int espacio = dameEspacioCamion();
+        float espacio = dameEspacioCamion();
         float [] objetos = dameObjetos();
         voraz(objetos, espacio);
     }
-    public static float [] voraz(float [] objetos, int capacidad){
+    public static float [] voraz(float [] objetos, float pmaximo){
         Arrays.sort(objetos);
-        int auxiliar = objetos.length;
-        float [] sol;
+        int tam = objetos.length;
+        float pactual=0; //Para saber el peso acumulado
+        float [] sol = new float[100];
         float x;
-        if(objetos[i])
+        int i=0;
         //Bucle voraz:
-        while(objetos.length<auxiliar && !esSolucion()){
-            x = seleccionar(objetos);
+        while(!esSolucion(i,pmaximo,pactual,tam)){
+            x = seleccionar(objetos,i);
+            if(factible(pmaximo,x,pactual)) {
+                pactual = pactual + x;
+                sol[tam-i] = x;
+            }
+            i++;
         }
         return sol;
     }
-    public static boolean esSolucion(){
-
+    public static boolean esSolucion(int i,float pmaximo,float pactual,int tam){
+        //será solución cuando no haya mas pesos o cuando se haya llenado
+        return pactual == pmaximo || i == tam;
     }
-    public static boolean factible(int espacio, float objetoActual,float pesoAcumuladoActual){
+    public static boolean factible(float espacio, float objetoActual,float pesoAcumuladoActual){
         return espacio >= pesoAcumuladoActual + objetoActual;
         //será factible únicamente si cabe aún en el camión
     }
-    public static float seleccionar(float[] objetos){
-        float x = objetos[1];
+    public static float seleccionar(float[] objetos,int i){
+        float x = objetos[i];
         return x;
     }
 }
