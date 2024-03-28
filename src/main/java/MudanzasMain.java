ee76ecbe1b3ec72de76054a8118f781f9b9cd3c2
mandp9 <MPilar.Ballesteros3@alu.uclm.es>
2024-03-28 12:53:25 +0100

    Quedan comentados los scanners y algunos cambios pequeños en voraz. He añadido i<tam como otra condición del while y algunos comentarios. Aún así sigue sin funcionar..

--- src/main/java/MudanzasMain.java
+++ src/main/java/MudanzasMain.java
@@ -1,12 +1,15 @@
 import java.util.*;
 
 public class MudanzasMain {
-    private static Scanner scanner = new Scanner(System.in);
+    //private static Scanner scanner = new Scanner(System.in);
 
+    /*
     public static float dameEspacioCamion(){
         System.out.println("Introduzca peso máximo que se podrá cargar en el camión: ");
         return scanner.nextFloat();
     }
+     */
+    /*
     public static float [] dameObjetos(){
         float [] objetos = new float[100];
         float n=0;
@@ -21,27 +24,36 @@ public class MudanzasMain {
 
         return objetos;
     }
+     */
     public static void main(String[] args) {
-        float espacio = dameEspacioCamion();
-        float [] objetos = dameObjetos();
-        System.out.println(Arrays.toString(voraz(objetos, espacio)));
+        float espacio = 100;
+        float [] objetos = {30,50, 20, 70,10};
+        System.out.println("Objetos a transportar: " + Arrays.toString(voraz(objetos, espacio)));
     }
     public static float [] voraz(float [] objetos, float pmaximo){
+        //ordenacion
+        System.out.println("Objetos a cargar en el camión: " + Arrays.toString(objetos));
         Arrays.sort(objetos);
+        System.out.println("Ordenación paquetes por peso creciente: " + Arrays.toString(objetos));
         int tam = objetos.length;
-        float pactual=0; //Para saber el peso acumulado
+        float pactual = 0; //Para saber el peso acumulado hasta ahora
         float [] sol = new float[tam];
-        float x;
-        int i=0;
+        float x; //auxiliar para ir seleccionando objetos del array objetos
+        int i = 0;
+        int factura = 0;
         //Bucle voraz:
-        while(!esSolucion(i,pmaximo,pactual,tam)){
+        while(!esSolucion(i,pmaximo,pactual,tam) && i < tam){
+            //seleccion
             x = seleccionar(objetos,i);
+            //factibilidad
             if(factible(pmaximo,x,pactual)) {
                 pactual = pactual + x;
                 sol[i] = x;
+                factura += 40;
             }
             i++;
         }
+        System.out.println("Factura coste por los productos transportados: " + factura);
         return sol;
     }
     public static boolean esSolucion(int i,float pmaximo,float pactual,int tam){
