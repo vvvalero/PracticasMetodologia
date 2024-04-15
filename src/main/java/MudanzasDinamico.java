994741880177649aecc53f2d3002126047811d66
vavaji <vavaji@alumni.uv.es>
2024-04-15 18:55:37 +0200

    Pruebas de Log4J

--- src/main/java/MudanzasDinamico.java
+++ src/main/java/MudanzasDinamico.java
@@ -2,7 +2,13 @@ import java.util.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 public class MudanzasDinamico {
+    private static final Logger logger = LogManager.getLogger(MudanzasDinamico.class);
     public static void main(String[] args) {
-
+        logger.trace("Mensaje TRACE: Muy detallado, ideal para diagnóstico fino");
+        logger.debug("Mensaje DEBUG: Detalles de depuración, útil durante el desarrollo");
+        logger.info("Mensaje INFO: Información general sobre el flujo de la aplicación");
+        logger.warn("Mensaje WARN: Advertencia sobre algo inusual, pero no crítico");
+        logger.error("Mensaje ERROR: Error en la ejecución que debe ser investigado");
+        logger.fatal("Mensaje FATAL: Error crítico que probablemente aborte la aplicación");
     }
 }
