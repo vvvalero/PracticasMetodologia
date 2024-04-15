2c47bb99d19f26f6c0b9318790c5b66b8c34f57d
vavaji <vavaji@alumni.uv.es>
2024-04-15 18:46:23 +0200

    He añadido los primeros imports

--- src/main/java/MudanzasDinámico.java
+++ src/main/java/MudanzasDinámico.java
@@ -1,2 +1,8 @@
+import java.util.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 public class MudanzasDinámico {
+    public static void main(String[] args) {
+
+    }
 }
--- src/test/java/MudanzasDinamicoTests.java
+++ src/test/java/MudanzasDinamicoTests.java
@@ -1,2 +1,6 @@
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class MudanzasDinamicoTests {
 }
