ff4e8e82c05f7de8209d27ee6a70116280b561c5
vavaji <vavaji@alumni.uv.es>
2024-04-15 18:47:42 +0200

    He añadido los primeros imports

--- src/main/java/MudanzasDinamico.java
+++ src/main/java/MudanzasDinamico.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-public class MudanzasDinámico {
+public class MudanzasDinamico {
     public static void main(String[] args) {
 
     }
