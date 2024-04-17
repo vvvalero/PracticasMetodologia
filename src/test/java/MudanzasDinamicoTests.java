b7eb38a868ad91b4dfb52f5a832782b6264fa76e
vavaji <vavaji@alumni.uv.es>
2024-04-17 11:53:52 +0200

    He añadido los primeros tests básicos.

--- src/test/java/MudanzasDinamicoTests.java
+++ src/test/java/MudanzasDinamicoTests.java
@@ -1,6 +1,18 @@
 import org.junit.jupiter.api.Test;
-import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MudanzasDinamicoTests {
+    @Test
+    public void testMain() {
+        assertTrue(true);
+    }
+
+    @Test
+    public void testFactura() {
+        assertEquals(120, MudanzasDinamico.factura(3, 40));
+    }
+
+    @Test
+    public void testFacturaNulo() {assertEquals(0, MudanzasDinamico.factura(0, 40));}
+
 }
