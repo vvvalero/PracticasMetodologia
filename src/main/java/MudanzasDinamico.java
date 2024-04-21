40db418c6710a396b55bc4a73691f7e070746ebd
alberto <>
2024-04-21 13:35:37 +0200

    Nose

--- src/main/java/MudanzasDinamico.java
+++ src/main/java/MudanzasDinamico.java
@@ -2,6 +2,8 @@ import java.util.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 public class MudanzasDinamico {
+    ArrayList<float> pesos=new ArrayList<float>();
+    float pmax;
     private static final Logger logger = LogManager.getLogger(MudanzasDinamico.class);
     public static int factura(int tam,int precio){
         return tam*precio;
@@ -15,4 +17,27 @@ public class MudanzasDinamico {
         logger.fatal("Mensaje FATAL: Error crítico que probablemente aborte la aplicación");
     }
 
+
+    //Debera llamar a matrizDecisiones y a sacarPesos
+    public static ArrayList<Float> algoritmoDinamico(ArrayList<Float> pesos, float capacidad){
+        return new ArrayList<>();
+    }
+    public static float[][] matrizDecisiones(ArrayList<Float> pesos,int pmax,float beneficio){
+        //[pesos.size()][pmax + 1]
+        int filas = pesos.size();
+        int col = pmax;
+        float[][] matriz = new float[filas][col];
+        for (int i = 0;i<filas;i++){
+            for(int j=0;j<col;j++){
+                if(i==0 || j==0)
+                    matriz[i][j]=0;
+                matriz[i][j]=Float.max(matriz[i-1][j],beneficio + matriz[i-1][(int) (j-pesos.get(i))]);
+            }
+        }
+        return matriz;
+    }
+    //Sacar los pesos usados en la matriz de Decisiones y devolverlos en un arraylist
+    public static ArrayList<Float> sacarPesos(ArrayList<Float> pesos, float capacidad){
+        return new ArrayList<>();
+    }
 }
--- src/test/java/MudanzasDinamicoTests.java
+++ src/test/java/MudanzasDinamicoTests.java
@@ -1,4 +1,7 @@
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MudanzasDinamicoTests {
@@ -15,4 +18,30 @@ public class MudanzasDinamicoTests {
     @Test
     public void testFacturaNulo() {assertEquals(0, MudanzasDinamico.factura(0, 40));}
 
+    @Test
+    public void testListaEstandar(){
+        //Variables a cambiar para modificar el test facilmente
+        float[] pesosA = {3,4,3,3,3};
+        float pmax = 9;
+        float[] esperado = {3,3,3};
+
+        //Pasar de float[] a ArrayList[Float] para la entrada de voraz()
+        ArrayList<Float> pesos = new ArrayList<>();
+        for (float p : pesosA) {
+            pesos.add(p);
+        }
+
+        //Pasar el resultado de voraz() de ArrayList<Float> a float[]
+        ArrayList<Float> res = MudanzasMainList.voraz(pesos, pmax);
+        float[] Res = new float[res.size()];
+        for (int i = 0; i<res.size(); i++) {
+            Res[i] = res.get(i);
+        }
+
+        //comprobacion que voraz() devuelve lo esperado:
+        assertArrayEquals(esperado, Res);
+        //comprobacion que factura() devuelve lo esperado:
+        assertEquals(esperado.length*40, MudanzasMainList.factura(Res.length, 40));
+    }
 }
+//Domain driven design
