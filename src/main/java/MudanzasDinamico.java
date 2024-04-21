b36f7e6e805313b46681bcb71cb652ed1cd41ec5
alberto <>
2024-04-21 14:37:29 +0200

    implementacion inicial (a medias)

--- src/main/java/MudanzasDinamico.java
+++ src/main/java/MudanzasDinamico.java
@@ -2,8 +2,8 @@ import java.util.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 public class MudanzasDinamico {
-    ArrayList<float> pesos=new ArrayList<float>();
-    float pmax;
+    ArrayList<Integer> pesos=new ArrayList<Integer>();
+    int pmax;
     private static final Logger logger = LogManager.getLogger(MudanzasDinamico.class);
     public static int factura(int tam,int precio){
         return tam*precio;
@@ -19,25 +19,42 @@ public class MudanzasDinamico {
 
 
     //Debera llamar a matrizDecisiones y a sacarPesos
-    public static ArrayList<Float> algoritmoDinamico(ArrayList<Float> pesos, float capacidad){
+    public static ArrayList<Integer> algoritmoDinamico(ArrayList<Integer> pesos, int pmax){
         return new ArrayList<>();
     }
-    public static float[][] matrizDecisiones(ArrayList<Float> pesos,int pmax,float beneficio){
+    public static int[][] matrizDecisiones(ArrayList<Integer> pesos,int pmax,int beneficio){
         //[pesos.size()][pmax + 1]
         int filas = pesos.size();
         int col = pmax;
-        float[][] matriz = new float[filas][col];
+        int[][] matriz = new int[filas][col];
         for (int i = 0;i<filas;i++){
             for(int j=0;j<col;j++){
                 if(i==0 || j==0)
                     matriz[i][j]=0;
-                matriz[i][j]=Float.max(matriz[i-1][j],beneficio + matriz[i-1][(int) (j-pesos.get(i))]);
+                else
+                    matriz[i][j]=Integer.max(matriz[i-1][j],beneficio + matriz[i-1][(j-pesos.get(i))]);
             }
         }
         return matriz;
     }
     //Sacar los pesos usados en la matriz de Decisiones y devolverlos en un arraylist
-    public static ArrayList<Float> sacarPesos(ArrayList<Float> pesos, float capacidad){
-        return new ArrayList<>();
+    public static ArrayList<Integer> sacarPesos(ArrayList<Integer> pesos, int pmax,int[][] matriz){
+        ArrayList<Integer> res = new ArrayList<>();
+        int filas = pesos.size();
+        int col = pmax;
+        int aux = matriz[filas-1][col-1];
+
+        //MUY ineficiente
+        for(int j=col-2;j>=0;j--){
+            for (int i=filas-2;i>=0;i++){
+                if(matriz[i][j] < aux){
+                    res.add(matriz[i+1][j]);
+                    i=i+1;
+                    j=j - pesos.get(j);
+                }
+                aux=matriz[i][j];
+            }
+        }
+        return res;
     }
 }
