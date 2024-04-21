7367afc7636ff8370eb06fb40d9045a98c23f602
vavaji <vavaji@alumni.uv.es>
2024-04-21 20:50:38 +0200

    He hecho el main y he puesto el logger de la solucion, pero no muestra nada por pantalla

--- src/main/java/MudanzasDinamico.java
+++ src/main/java/MudanzasDinamico.java
@@ -2,19 +2,19 @@ import java.util.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 public class MudanzasDinamico {
-    ArrayList<Integer> pesos=new ArrayList<Integer>();
-    int pmax;
+    private static ArrayList<Integer> pesos = new ArrayList<Integer>();
     private static final Logger logger = LogManager.getLogger(MudanzasDinamico.class);
     public static int factura(int tam,int precio){
         return tam*precio;
     };
     public static void main(String[] args) {
-        logger.trace("Mensaje TRACE: Muy detallado, ideal para diagnóstico fino");
-        logger.debug("Mensaje DEBUG: Detalles de depuración, útil durante el desarrollo");
-        logger.info("Mensaje INFO: Información general sobre el flujo de la aplicación");
-        logger.warn("Mensaje WARN: Advertencia sobre algo inusual, pero no crítico");
-        logger.error("Mensaje ERROR: Error en la ejecución que debe ser investigado");
-        logger.fatal("Mensaje FATAL: Error crítico que probablemente aborte la aplicación");
+        int pmax = 15,beneficio = 6; //casos que da el problema
+        int[] pesosA = {3,4,3,3,3}; //se puede modificar como queramos
+        for (int p : pesosA) { //convertir el array a un arraylist
+            pesos.add(p);
+        }
+        ArrayList<Integer> sol= new ArrayList<Integer>(algoritmoDinamico(pesos,pmax,beneficio));
+        logger.info("Solucion: "+sol); //no se si se puede hacer un logger.info(sol) directamente
     }
 
 
