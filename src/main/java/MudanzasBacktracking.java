386378250066d3a2164400c714b0063b26fddc4c
vavaji <vavaji@alumni.uv.es>
2024-05-02 13:20:01 +0200

    Añadidas los dos archivos de la práctica 3(vacios)

---
+++ src/main/java/MudanzasBacktracking.java
@@ -0,0 +1,11 @@
+import MetodosAdicionales.MetodosAdicionales;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MudanzasBacktracking {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/test/java/MudanzasBacktrackingTests.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+import static org.junit.jupiter.api.Assertions.*;
+import operaciones.ArrayMatriz;
+public class MudanzasBacktrackingTests {
+    public static void main(String[] args) {
+
+    }
+}
