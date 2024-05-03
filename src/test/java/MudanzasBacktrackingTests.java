2f0ce72d202d477f4ff9235d09303e7c9a6022a9
vavaji <vavaji@alumni.uv.es>
2024-05-03 17:06:13 +0200

    Practica 3: Inicio tests

--- src/test/java/MudanzasBacktrackingTests.java
+++ src/test/java/MudanzasBacktrackingTests.java
@@ -3,7 +3,6 @@ import java.util.ArrayList;
 import static org.junit.jupiter.api.Assertions.*;
 import operaciones.ArrayMatriz;
 public class MudanzasBacktrackingTests {
-    public static void main(String[] args) {
-
-    }
+    @Test
+    public void testMain() {assertTrue(true);}
 }
