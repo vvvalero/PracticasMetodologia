6fa7a5a5df54acf213ef5ce2ed1f46150be12618
vavaji <vavaji@alumni.uv.es>
2024-05-03 18:46:16 +0200

    He hecho una primera aproximacion del algoritmo, ninguna prueba

--- src/main/java/MudanzasBacktracking.java
+++ src/main/java/MudanzasBacktracking.java
@@ -3,9 +3,51 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 
 public class MudanzasBacktracking {
-    public static void main(String[] args) {
+    private static final Logger logger = LogManager.getLogger(MudanzasBacktracking.class);
+    //variables globales
+    private static ArrayList<Integer> pesos = new ArrayList<>();
+    static int pmax = 20000; //casos que da el problema
+    static ArrayList<Integer> x = new ArrayList<>(); //vector solucion, lo he llamado x para seguir el pseudocodigo
 
+
+    public static void main(String[] args) {
+        int[] pesosA = {3,5,8}; //se puede modificar como queramos
+        for (int p : pesosA) { //convertir el array a un arraylist
+            pesos.add(p);
+        }
+        if (pmax < 0) {
+            logger.warn("El peso maximo es negativo");
+            throw new RuntimeException("Peso maximo negativo");
+        }
+        int k=0;
+        ArrayList<Integer> sol= new ArrayList<>(Mochila(pesos,k));
+    }
+
+    public static ArrayList<Integer> Mochila(ArrayList<Integer> pesos,int k) {
+        ArrayList<Integer> y = new ArrayList<>();
+
+        for (int c=1; c<=3; c++) {
+            x.add(c);
+            if(vivo()){
+                if(k==pesos.size()-1)
+                    y=x;
+                else
+                    Mochila(pesos,k+1);
+            }
+        }
+        return x;
+    }
+    public static boolean vivo(){
+        return (verPeso(pesos) <= pmax);
+    }
+    private static int verPeso(ArrayList<Integer> pesos){
+        int peso = 0;
+        for (int i = 0; i < x.size(); i++) {
+            peso += x.indexOf(i) * pesos.get(i);
+        }
+        return peso;
     }
 }
